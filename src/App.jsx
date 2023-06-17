cea167528a2df0ca5e86e3dc51623241a31d73d0
Nat_FL <ngraddy@ngraddy-ltmyx9z.internal.salesforce.com>
2023-06-17 13:03:53 -0700

    Completed Step 1 Part 2 of lab-implementing part of userProfile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, {useState} from "react"
 import Feed from "./components/Feed/Feed"
 import Navbar from "./components/Navbar/Navbar"
 import UserProfile from "./components/UserProfile/UserProfile"
@@ -6,15 +7,18 @@ import { codepathUserProfile, firstTweet, navLinks } from "./constants"
 
 export default function App() {
 
-  console.log({navLinks})
+  
+  const [userProfile, setUserProfile] = useState(codepathUserProfile)
 
+  console.log({navLinks})
+  console.log(userProfile)
   return (
     <div className="app">
       {/* creates new prop/argument that takes in navLinks properties
       navLinks is an array of objects */}
       <Navbar navInfo = {navLinks}/> 
       <main>
-        <UserProfile />
+        <UserProfile userProfile ={userProfile} />
         <Feed />
         <Advertisements />
       </main>
--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -2,18 +2,19 @@ import { formatNumTweets, formatNumFollowers } from "../../utils/format"
 import "./UserProfile.css"
 
 export default function UserProfile({ userProfile }) {
+  console.log(userProfile)
   return (
     <div className="col user-profile">
       <div className="card">
         <div className="card-bg" />
-        <CardContent />
-        <CardFooter />
+        <CardContent cardInfo = {userProfile} />
+        <CardFooter props = {userProfile}/>
       </div>
     </div>
   )
 }
 
-export function CardContent(props) {
+export function CardContent({cardInfo}) {
   return (
     <div className="card-content">
       <span className="fa-stack add-picture-icon">
@@ -22,14 +23,14 @@ export function CardContent(props) {
         </i>
       </span>
       <div className="twitter-handle">
-        <h3>{props.name}</h3>
-        <p>@{props.handle}</p>
+        <h3>{cardInfo.name}</h3>
+        <p>@{cardInfo.handle}</p>
       </div>
     </div>
   )
 }
 
-export function CardFooter(props) {
+export function CardFooter({props}) {
   return (
     <div className="card-footer">
       <p>Tweets</p>
